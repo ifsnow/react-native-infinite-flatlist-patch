f3ec64be7be1c9d058d98eb8b3a1bd3fcbd57699
ifsnow <ifsnow@gmail.com>
2020-04-05 23:31:44 +0900

    Fixed compatibility with RN 0.62

--- patch.js
+++ patch.js
@@ -14,24 +14,38 @@ const minorVersion = parseInt(version[1], 10);
 const patchVersion = parseInt(version[2], 10);
 
 let patchDir = '';
-if (minorVersion >= 60) {
-  patchDir = '0.60.5';
-} else if (minorVersion === 59) {
-  if (patchVersion >= 8) {
-    patchDir = '0.59.8';
-  } else if (patchVersion >= 5) {
-    patchDir = '0.59.5';
-  } else if (patchVersion >= 3) {
-    patchDir = '0.59.3';
-  }
-} else if (minorVersion === 58) {
-  patchDir = '0.58.6';
-} else if (minorVersion === 56 && minorVersion === 57) {
-  patchDir = '0.57.8';
-} else if (minorVersion === 55) {
-  patchDir = '0.55.4';
-} else if (minorVersion === 54) {
-  patchDir = '0.54.4';
+switch (minorVersion) {
+  case 62:
+    patchDir = '0.62.1';
+  case 61:
+    patchDir = '0.61.5';
+  case 60: 
+    patchDir = '0.60.5';
+    break;
+  case 59:
+    if (patchVersion >= 8) {
+      patchDir = '0.59.8';
+    } else if (patchVersion >= 5) {
+      patchDir = '0.59.5';
+    } else if (patchVersion >= 3) {
+      patchDir = '0.59.3';
+    }
+    break;
+  case 58:
+    patchDir = '0.58.6';
+    break;
+  case 57:
+  case 56:
+    patchDir = '0.57.8';
+    break;    
+  case 55:
+    patchDir = '0.55.4';
+    break;
+  case 54:
+    patchDir = '0.54.4';
+    break;
+  default:    
+    break;
 }
 
 if (patchDir === '') {
